829021e11508880c73f0eda999902c4ab94471f3
imnotsatosh <imnotsatosh@proton.me>
2024-03-08 16:03:38 +0800

    modify configure

--- src/configuration.h
+++ src/configuration.h
@@ -64,13 +64,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define RANDOMX_PROGRAM_COUNT 2
 
 // Scratchpad L3 size in bytes. Must be a power of 2.
-#define RANDOMX_SCRATCHPAD_L3 1048576*2
+#define RANDOMX_SCRATCHPAD_L3 1048576/2
 
 // Scratchpad L2 size in bytes. Must be a power of two and less than or equal to RANDOMX_SCRATCHPAD_L3.
-#define RANDOMX_SCRATCHPAD_L2 262144*2
+#define RANDOMX_SCRATCHPAD_L2 262144
 
 // Scratchpad L1 size in bytes. Must be a power of two (minimum 64) and less than or equal to RANDOMX_SCRATCHPAD_L2.
-#define RANDOMX_SCRATCHPAD_L1 16384*4
+#define RANDOMX_SCRATCHPAD_L1 16384
 
 // Jump condition mask size in bits.
 #define RANDOMX_JUMP_BITS 8
